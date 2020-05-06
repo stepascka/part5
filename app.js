9f076b34809d50cb79cbc7661dc1cf61b45d5c9f
stepascka <rinatik.m@mail.ru>
2020-05-06 12:10:03 +0300

    span pressed

--- app.js
+++ app.js
@@ -23,19 +23,61 @@ var main = function () {
 			else if ($element.parent().is(":nth-child(2)")) {
 				$content = $("<ul>");
 				toDos.forEach(function (todo) {
-					$content.append($("<li>").text(todo));
+					$content.append($("<li>").text(todo));					
 				});
 				$("main .content").append($content);
 			} 
 			else if ($element.parent().is(":nth-child(3)")) {
-				$content = $("<input>");
-				$("main .content").append($content);
-				$content = $("<button>").text("+");
-				$("main .content").append($content);
+					var tags = [];
+					toDoObjects.forEach(function (toDo) {
+						toDo.tags.forEach(function (tag) {
+							if (tags.indexOf(tag) == -1) {
+								tags.push(tag);
+							}
+						});
+					});
 
-				$("button").on("click", function (event) {
-					//console.log($("input").val());
-					toDos.push($("input").val());
+					var tagObjects = tags.map(function (tag) {
+						var toDosWithTag = [];
+						toDoObjects.forEach(function (toDo) {
+							if (toDo.tags.indexOf(tag) != -1) {
+							toDosWithTag.push(toDo.description);
+							}
+						});
+						return { "name": tag, "toDos": toDosWithTag };
+					});
+
+				tagObjects.forEach(function (tag) {
+					var $tagName = $("<h3>").text(tag.name),
+					$content = $("<ul>");
+					tag.toDos.forEach(function (description) {
+						var $li = $("<li>").text(description);
+						$content.append($li);
+					});
+					$("main .content").append($tagName);
+					$("main .content").append($content);
+				});
+			} 
+			else if ($element.parent().is(":nth-child(4)")) {				
+				var $inputLabel = $("<p>").text("Новая задача: ");
+				$("main .content").append($inputLabel);
+				var $input = $("<input>").addClass("description");
+				$("main .content").append($input);				
+				var $tagLabel = $("<p>").text("Тэги: ");
+				$("main .content").append($tagLabel);
+				var $tagInput = $("<input>").addClass("tags");
+				$("main .content").append($tagInput);
+				var $button = $("<button>").text("+");
+				$("main .content").append($button);
+				$button.on("click", function () {
+					var description = $input.val();
+					var tags = $tagInput.val().split(",");
+					toDoObjects.push({"description":description, "tags":tags});
+					toDos = toDoObjects.map(function (toDo) {
+						return toDo.description;
+					});
+					$input.val("");
+					$tagInput.val("");
 				});
 			}
 
